eefaadfbbac594cd97d6fc7dea14d419035af2b3
juliocesar1415 <juliocesarenge@gmail.com>
2021-07-08 21:26:07 -0300

    projeto atualizado
    projeto cilindro atualizado

---
+++ src/projetovolumedocilindro/cilindro.java
@@ -0,0 +1,34 @@
+
+package projetovolumedocilindro;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+ public class cilindro {
+
+    private static Scanner sc;
+    
+     public static void main(String[] args) {
+     
+         
+         Scanner  sc = new Scanner(System.in);
+    
+    
+   
+    
+  
+    
+    System.out.println("`digite o valor da altura em metros:");
+           double altura = sc.nextDouble();
+
+    System.out.println("digite o valor do raio em metros:");
+           double raio= sc.nextDouble();
+           
+    System.out.println("o volume do cilindro em metros cubicos é "+(raio*raio*3.14*altura));
+           
+          }
+           
+     }
+
+
